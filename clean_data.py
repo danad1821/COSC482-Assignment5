4b8fab1abd81147c4c2d66d6f66510060b35e4eb
danad1821 <danadabdoub@gmail.com>
2025-03-19 18:42:14 +0200

    started cleaning

---
+++ clean_data.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import re
+import numpy as np
+
+if __name__ == "__main__":
+    df = pd.read_csv("ebay_tech_deals.csv")
+    
+    def extract_price(price_string):
+        if isinstance(price_string, str):  # Check if the value is a string
+            match = re.search(r"[\d.]+", price_string)
+            if match:
+                try:
+                    return float(match.group(0))
+                except ValueError:
+                    return None
+            else:
+                return None
+        else:
+            return None  # Return None for non-string values
+
+    df['price'] = df['price'].apply(extract_price)
+    df['original_price'] = df['original_price'].apply(extract_price)
+    
+    df["original_price"] = df["original_price"].fillna(df["price"])
+    
+    df["shipping"] = df["shipping"].fillna("Shipping info unavailable")
+    
+    df["price"] = pd.to_numeric(df['price'], errors="coerce")
+    df["original_price"] = pd.to_numeric(df['original_price'], errors="coerce")
+    df["discount_percentage"] = ((1 - (df["price"]/df["original_price"]))*100).round(2)
+    
+    df.to_csv("cleaned_ebay_deals.csv")
+    
+    
